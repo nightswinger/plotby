cc12e97413481bf7748acd237ca1be95b2c6201f
nightswinger <stardustkids83@gmail.com>
2020-12-08 10:10:21 +0000

    add autoload path for BaseChartclass

--- lib/plotby/charts.rb
+++ lib/plotby/charts.rb
@@ -1,3 +1,4 @@
 module Plotby::Charts
+  autoload :BaseChart, "plotby/charts/base_chart.rb"
   autoload :LineChart, "plotby/charts/line_chart.rb"
 end
--- lib/plotby/version.rb
+++ lib/plotby/version.rb
@@ -1,3 +1,3 @@
 module Plotby
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
